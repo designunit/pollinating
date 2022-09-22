92503dcebab461b0c61308eeb06de7d8517e9c60
Roman Timashev <roman@tmshv.ru>
2022-09-22 19:47:55 +0300

    hide

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,21 @@
 import Head from 'next/head'
-import { YMetrika } from '@/components/YMetrika'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 
 export default function Home() {
     const metrika = process.env.NEXT_PUBLIC_YANDEX_METRIKA
 
     return (
-        <div className={styles.container}>
+        <div className={}>
             <Head>
-                <title>мойдербент.рф</title>
+                <title>pollinating</title>
                 <link rel="icon" href="/favicon.ico" />
-
-                {!metrika ? null : (
-                    <YMetrika
-                        number={metrika}
-                        mode={'script'}
-                    />
-                )}
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    мойдербент.рф
+            <main className={}>
+                <h1 className={}>
+                  pollinating.space
                 </h1>
             </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://unit4.io"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    design::unit
-                </a>
-            </footer>
         </div>
     )
 }
