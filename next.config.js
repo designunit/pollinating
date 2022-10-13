a1bf390e8f3a6b6c3bf432334673e7c834f52d69
Roman Timashev <roman@tmshv.ru>
2022-10-13 21:46:11 +0600

    next config format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     i18n: {
-        locales: ['ru', 'en', 'de'],
-        defaultLocale: 'ru'
-    }
-};
+        locales: ["ru", "en", "de"],
+        defaultLocale: "ru",
+    },
+}
